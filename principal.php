f77e9b49e64867d7aee6159a008e87b347439e1b
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-05-18 18:32:55 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -1,57 +1,133 @@
-<div class="row">
-    <h2 class="page-header">Bem-vindo ao meu site</h2>
-    <div class="col-md-12">
-      <div class="panel">
-        <div class="panel-heading">
-          <h3>Aqui você irá encontrar de tudo.</h3> 
-        </div>
-        <div class="col-md-6 col-serv">
-          <h4>Serviços</h4>
-<p>
-Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser entendido como manifestação linguística das ideias de um autor, que serão interpretadas pelo leitor de acordo com seus conhecimentos linguísticos e culturais. Seu tamanho é variável.
 
-“Conjunto de palavras e frases articuladas, escritas sobre qualquer suporte”.[1]
+<style>
+ p01{
+  font: normal 26px "Bookman Old Style";
+ }
+ p02{
+  font: normal 23px "Bookman Old Style";
+ }
+</style>
 
-“Obra escrita considerada na sua redação original e autêntica (por oposição a sumário, tradução, notas, comentários, etc.)”.[2]
-
-"Um texto é uma ocorrência linguística, escrita ou falada de qualquer extensão, dotada de unidade sociocomunicativa, semântica e formal. É uma unidade de linguagem em uso."[3]
-
-O interesse pelo texto como objeto de estudo gerou vários trabalhos importantes de teóricos da Linguística Textual, que percorreram fases diversas cujas características principais eram transpor os limites da frase descontextualizada da gramática tradicional e ainda incluir os relevantes papéis do autor e do leitor na construção de textos.
-
-Um texto pode ser escrito ou oral e, em sentido lato, pode ser também não verbal.
-
-Texto crítico é uma produção textual que parte de um processo reflexivo e analítico gerando um conteúdo com crítica construtiva e bem fundamentada.
-
-Em artes gráficas, o texto é a parte verbal, linguística, por oposição às ilustrações.
-
-Todo texto tem que ter alguns aspectos formais, ou seja, tem que ter estrutura, elementos que estabelecem relação entre si. Dentro dos aspectos formais temos a coesão e a coerência, que dão sentido e forma ao texto. "A coesão textual é a relação, a ligação, a conexão entre as palavras, expressões ou frases do texto”.[4] A coerência está relacionada com a compreensão, a interpretação do que se diz ou escreve. Um texto precisa ter sentido, isto é, precisa ter coerência. Embora a coesão não seja condição suficiente para que enunciados se constituam em textos, são os elementos coesivos que lhes dão maior legibilidade e evidenciam as relações entre seus diversos componentes, a coerência depende da coesão.
-
-Textos literários e não literários
-Os textos literários são aqueles que, em geral, têm o objetivo de emocionar o leitor, e para isso exploram a linguagem conotativa ou poética. Em geral, ocorre o predomínio da função emotiva e poética.
-
-Exemplos de textos literários: poemas, romances literários, contos, telenovelas.
-
-Os textos não literários pretendem informar o leitor de forma direta e objetiva, a partir de uma linguagem denotativa. A função referencial predomina-se nos textos não-literários.
-
-Exemplos de textos não-literários: notícias e reportagens jornalísticas, textos de livros didáticos de História, Geografia, Ciências, textos científicos em geral, receitas culinárias, bulas de remédio.
-
-Referências
- CAMARGO e BELLOTTO, 1996:74.
- AURÉLIO.
- COSTA VAL, 1991.
- PLATÃO & FIORIN, 1996.
-Bibliografia
-AURÉLIO. Veja-se FERREIRA, Aurélio Buarque de Holanda. Dicionário Aurélio eletrônico – Século XXI.
-COSTA VAL, M. da Graça. Redação e textualidade. São Paulo, Martins Fontes, 1991.
-CAMARGO, Ana Maria de Almeida; BELLOTTO, Heloísa Liberalli (orgs.). Dicionário de terminologia arquivística. São Paulo: Associação dos Arquivistas Brasileiros – Núcleo de São Paulo / Secretaria de Estado da Cultura – Departamento de Museus e Arquivos, 1996.
-FERREIRA, Aurélio Buarque de Holanda. Dicionário Aurélio eletrônico – Século XXI. Versão 3.0. São Paulo: Lexikon, 1999.
-KOCH, Ingedore Grunfeld Villaça; TRAVAGLIA, Luis Carlos. Texto e coerência. 8.e
-</p>
-        </div>
-        <div class="col-md-6 col-proj">
-          <h4>Projetos</h4>
-            <p>texto projetos</p>
-        </div>
-      </div>    
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-7">
+      <div class="panel-heading">
+        <h3><a name="O que é COVID-19">O que é COVID-19</a></h3> 
+        <hr>
+      </div>
+    </div>
+    <div class="col-1">
+    </div>
+    <div class="col-4">
+      <div class="panel-heading">
+        <h3 style="text-align: center">Última Atualização</h3> 
+        <hr>
+      </div>
     </div>
   </div>
+  <div class="row">
+    <div class="col-7">
+      <p>   
+      A COVID-19 é uma doença causada pelo coronavírus SARS-CoV-2, que apresenta um quadro 
+      clínico que varia de infecções assintomáticas a quadros respiratórios graves. <br>
+      De acordo com a Organização Mundial de Saúde (OMS), a maioria dos pacientes com COVID-19 (cerca de 80%)
+      podem ser assintomáticos e cerca de 20% dos casos podem requerer atendimento hospitalar por 
+      apresentarem dificuldade respiratória e desses casos aproximadamente 5% podem necessitar de
+      suporte para o tratamento de insuficiência respiratória (suporte ventilatório)
+      <br><br>
+
+      <h4>O que é o coronavírus?</h4>
+      Coronavírus é uma família de vírus que causam infecções respiratórias. O novo agente do coronavírus
+      foi descoberto em 31/12/19 após casos registrados na China. Provoca a doença chamada de coronavírus 
+      (COVID-19). Os primeiros coronavírus humanos foram isolados pela primeira vez em 1937. No entanto, 
+      foi em 1965 que o vírus foi descrito como coronavírus, em decorrência do perfil na microscopia,
+      parecendo uma coroa.A maioria das pessoas se infecta com os coronavírus comuns ao longo da vida, sendo 
+      as crianças pequenas mais propensas a se infectarem com o tipo mais comum do vírus. Os coronavírus mais 
+      comuns que infectam humanos são o alpha coronavírus 229E e NL63 e beta coronavírus OC43, HKU1.
+      <br><br>
+
+      <h4><a name="Quais são os sintomas">Quais são os sintomas</a></h4>
+      Os sintomas da COVID-19 podem variar de um simples resfriado até uma pneumonia severa.
+      Sendo os sintomas mais comuns:
+      <br>
+        &#9657; Tosse <br>
+        &#9657; Febre <br>
+        &#9657; Coriza <br>
+        &#9657; Dor de garganta <br>
+        &#9657; Dificuldade para respirar <br>
+      <br><br>
+
+      <h4><a name="Como é transmitido">Como é transmitido</a></h4>
+      A transmissão acontece de uma pessoa doente para outra ou por contato próximo por meio de: 
+      <br>
+       &#9657; Toque do aperto de mão <br>
+       &#9657; Gotículas de saliva <br>
+       &#9657; Espirro <br>
+       &#9657; Tosse <br>
+       &#9657; Catarro <br>
+       &#9657; Objetos ou superfícies contaminadas, como celulares, mesas, maçanetas, brinquedos, 
+       teclados de computador etc.
+      <br><br>
+
+      <h4><a name="Diagnóstico">Diagnóstico</a></h4>
+      O diagnóstico da COVID-19 é realizado primeiramente pelo profissional de saúde que deve avaliar a
+      presença de critérios clínicos: Pessoa com quadro respiratório agudo, caracterizado por sensação 
+      febril ou febre, que pode ou não estar presente na hora da consulta (podendo ser relatada ao 
+      profissional de saúde), acompanhada de tosse OU dor de garganta OU coriza OU dificuldade respiratória,
+      o que é chamado de Síndrome Gripal. Pessoa com desconforto respiratório/dificuldade para respirar OU
+      pressão persistente no tórax OU saturação de oxigênio menor do que 95% em ar ambiente OU coloração 
+      azulada dos lábios ou rosto, o que é chamado de Síndrome Respiratória Aguda Grave
+      Caso o paciente apresente os sintomas, o profissional de saúde poderá solicitar exame laboratoriais:
+      De biologia molecular (RT-PCR em tempo real) que diagnostica tanto a COVID-19, a Influenza ou a presença 
+      e Vírus Sincicial Respiratório (VSR).
+      Imunológico (teste rápido) que detecta, ou não, a presença de anticorpos em amostras coletadas somente 
+      após o sétimo dia de início dos sintomas.
+      O diagnóstico da COVID-19 também pode ser realizado a partir de critérios como: histórico de contato 
+      próximo ou domiciliar, nos últimos 7 dias antes do aparecimento dos sintomas, com caso confirmado 
+      laboratorialmente para COVID-19 e para o qual não foi possível realizar a investigação laboratorial 
+      específica, também observados pelo profissional durante a consulta.
+      <br><br>
+
+      <h4><a name="Como se proteger">Como se proteger</a></h4>
+      As recomendações de prevenção à COVID-19 são as seguintes:
+      <br>
+      &#9657; Lave com frequência as mãos até a altura dos punhos, com água e sabão, ou então higienize com álcool em gel 70%. <br>
+      &#9657; Ao tossir ou espirrar, cubra nariz e boca com lenço ou com o braço, e não com as mãos. <br>
+      &#9657; Evite tocar olhos, nariz e boca com as mãos não lavadas. <br>
+      &#9657; Ao tocar, lave sempre as mãos como já indicado. <br>
+      &#9657; Mantenha uma distância mínima de cerca de 2 metros de qualquer pessoa tossindo ou espirrando. <br>
+      &#9657; Evite abraços, beijos e apertos de mãos. Adote um comportamento amigável sem contato físico, mas sempre com um sorriso no rosto. <br>
+      &#9657; Higienize com frequência o celular e os brinquedos das crianças. <br>
+      &#9657; Não compartilhe objetos de uso pessoal, como talheres, toalhas, pratos e copos. <br>
+      &#9657; Mantenha os ambientes limpos e bem ventilados. <br>
+      &#9657; Evite circulação desnecessária nas ruas, estádios, teatros, shoppings, shows, cinemas e igrejas. Se puder, fique em casa. <br>
+      &#9657; Se estiver doente, evite contato físico com outras pessoas, principalmente idosos e doentes crônicos, e fique em casa até melhorar. <br>
+      &#9657; Durma bem e tenha uma alimentação saudável. <br>
+      &#9657; Utilize máscaras caseiras ou artesanais feitas de tecido em situações de saída de sua residência. <br>
+      </p>
+    </div>
+
+    <div class="col-1">
+    </div>
+
+    <div class="col-4" style="border-style: double;  height: 360px;">
+    <center>
+    <p01>
+    Horário <br>
+    <p02>17/05/2020 20:25</p02>
+    <hr>
+    Confirmados <br>
+    <p02>241.080</p02> 
+    <hr>
+    Recuperados <br>
+    <p02>94.122</p02> 
+    <hr>
+    Mortos <br>
+    <p02>16.118</p02> 
+    </p01>
+    <center>
+    </div>
+          
+  </div>
+
+</div>

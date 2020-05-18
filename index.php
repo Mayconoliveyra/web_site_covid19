124c25e2277b809592c8fe178493774227110d37
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-05-18 18:31:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,48 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-BR">
 <head>
-    <title>Olyveira.com</title>
-    <link rel="stylesheet" type="text/css" 
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Covid-19: Princípal</title>
+
+	<link rel="stylesheet" type="text/css" 
           href="bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="bootstrap/css/novocss.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/novocss.css">
+
+	<style type="text/css">
+		divTopo{
+		border: 1px solid #000;   
+		min-height: 70px;
+		background-color: rgba(207, 7, 7, 0.685);
+		color: rgba(207, 7, 7, 0.685);
+		}
+	</style>
 </head>
+
 <body>
 	<?php
-	include_once('topo.php');
-	?>
+		include_once('topo.php');
+	?>		
 	
 	<div class='container'>
-	<?php
-	# área de conteúdo
-	if(empty($_SERVER["QUERY_STRING"])){
-	        $var = "principal.php";
-	        include_once("$var");
-	}else{
-	        $pg = $_GET['pg'];
-	        include_once("$pg.php");
-	}
-	?>
+
+		<?php
+		#área de conteúdo
+		if(empty($_SERVER["QUERY_STRING"])){
+				$var = "principal.php";
+				include_once("$var");
+		}else{
+				$pg = $_GET['pg'];
+				include_once("$pg.php");
+		}
+		?>
+
 	</div>
+		
 	<?php
+		/* CHAMA PAG RODAPE*/
 		include_once('rodape.php');
 	?>
+
 </body>
 </html>

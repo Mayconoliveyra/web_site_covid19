d819874854615b2fc13038e03850d2b2c36b21b1
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-05-18 18:32:00 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,31 +1,55 @@
-<nav class="navbar-center">
-  <div class="container">
-    
+<html>
+<head>
+  <style>
+    pText1{
+        font: normal 14pt Segoe UI Semibold;
+        color:  rgb(211, 211, 235);
+    }
+
+    divMenu{
+       border: 2px solid rgba(255, 255, 255);   
+       background-color: rgb(18, 15, 26);
+       min-height: 35px;
+       text-align:  center;
+      
+      }
+
+  </style>
+
+</head>
+
+<body>
+    <!-- Menu -->
+    <div class="container-fluid">
     <div class ="row">
-      <div class="col-2">
-        <li><a href="?pg=principal">HARDWARE</a></li>
-      </div>
+    
+      <divMenu class="col-2">
+        <a href="#O que é COVID-19"> <pText1>Covid-19</pText1> </a>
+      </divMenu>
+    
+      <divMenu class="col-2">
+        <a href="#Quais são os sintomas"> <pText1>Sitomas</pText1> </a>
+      </divMenu>
 
-      <div class="col-2">
-        <li><a href="?pg=quemsomos">COMPUTADORES</a></li>
-      </div>
+      <divMenu class="col-2">
+        <a href="#Como é transmitido"> <pText1>Transmição</pText1> </a>
+      </divMenu>
 
-      <div class="col-2">
-        <li><a href="?pg=produtos">MONITORES</a></li>
-      </div>
+      <divMenu class="col-2">
+        <a href="#Diagnóstico"> <pText1>Diagnóstico</pText1> </a>
+      </divMenu>
 
-      <div class="col-2">
-        <li><a href="?pg=faleconosco">TECLADOS</a></li>
-      </div>
+      <divMenu class="col-2">
+        <a href="#Como se proteger"> <pText1>Prevenção</pText1> </a>
+      </divMenu>
 
-      <div class="col-2">
-        <li><a href="?pg=faleconosco">MOUSES</a></li>
-      </div>
+      <divMenu class="col-2">
+        <a href="?pg=memorial"> <pText1>Memorial</pText1> </a>
+      </divMenu>
 
-      <div class="col-2">
-        <li><a href="?pg=faleconosco">SMARTPHONES</a></li>
-      </div>
     </div>
-  
   </div>
-</nav>
+  <br> <br>
+</body>
+</html>
+

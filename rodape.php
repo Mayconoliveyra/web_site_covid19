4e7540796a62b97edd3d23e8ad93604cb227142a
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-05-18 18:34:04 -0300

    Update rodape.php

--- rodape.php
+++ rodape.php
@@ -2,33 +2,25 @@
 <style>
     p1{
         font: normal 9pt Segoe UI Semibold;
-        text-align: center;
-        color: rgba(5, 5, 5, 0.685);
+        color: rgba(255, 255, 255, 0.80);
     }
 
 </style>
-<style type="text/css">
-    div1{
-       border: 0px solid #000;   
-       min-height: 70px;
-       background-color: rgba(230, 237, 238, 0.685);
-    }
-</style>
-    <div class="container-fluid">
-        <div1 class="row">
-            <div1 class="col-12">          
-                <center>
-                <p1>
+<hr>
+    <div class="container-fluid" style="background-color: rgba(17, 23, 56, 0.685);">
+        <div class="row">
+            <div class="col-12">  
+               <center>
+               <p1>
                 <br>
-                R. Empresário Paulo Miranda de Oliveira - Portal do Sol- João Pessoa - PB, 58033-455 
-                <br><br><br>
-                Formas de pagamento aceitas: cartões de crédito (Visa, MasterCard, Elo e American Express), cartões de débito (Itaú, Santander, Bradesco e Banco do Brasil) e boleto.
-                <br><br><br><br>
-                OLIVEYRA COMÉRCIO ELETRÔNICO S/A | CNPJ: 07.770.777/0007-77 | Todos os direitos reservados.
-                <br><br>
+                Maycon Deyvid Brito de Oliveria [&copy;OLIVEYRA]  |  Todos os direitos reservados.
+                <br> <br>
+                Contato:(83)9.9967-5920   |   E-mail: mayconbrito1998@hotmail.com
+                <br> <br>
                 </p1>
-                </center>
-            </div1>
-        </div1>
+               
+               </center>
+            </div>
+        </div>
     </div>
 </footer>

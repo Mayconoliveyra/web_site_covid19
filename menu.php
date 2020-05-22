2eb5b19eb3852e2e409b7884a49d0e036f1f400e
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-05-21 22:43:37 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,24 +1,25 @@
+  
   <div class="container-fluid" style="background-color: rgba(203, 212, 205);">
     <div class ="row">
     
       <divMenu01 class="col-2">
-      <a href="#O que é COVID-19"> <pMenu01>COVID-19</pMenu01> </a>
+      <a href="/ProjetoWeb/#O%20que%20é%20COVID-19"> <pMenu01>COVID-19</pMenu01> </a>
       </divMenu01>
 
       <divMenu01 class="col-2">
-        <a href="#Quais são os sintomas"> <pMenu01>SITOMAS</pMenu01> </a>
+        <a href="/ProjetoWeb/#Quais%20são%20os%20sintomas"> <pMenu01>SITOMAS</pMenu01> </a>
       </divMenu01>
 
       <divMenu01 class="col-2">
-        <a href="#Como é transmitido"> <pMenu01>TRANSMISSÃO</pMenu01> </a>
+        <a href="/ProjetoWeb/#Como%20é%20transmitido"> <pMenu01>TRANSMISSÃO</pMenu01> </a>
       </divMenu01>
 
       <divMenu01 class="col-2">
-        <a href="#Diagnóstico"> <pMenu01>DIAGNÓSTICO</pMenu01> </a>
+        <a href="/ProjetoWeb/#Diagnóstico"> <pMenu01>DIAGNÓSTICO</pMenu01> </a>
       </divMenu01>
 
       <divMenu01 class="col-2">
-        <a href="#Como se proteger"> <pMenu01>PREVENÇÃO</pMenu01> </a>
+        <a href="/ProjetoWeb/#Como%20se%20proteger"> <pMenu01>PREVENÇÃO</pMenu01> </a>
       </divMenu01>
 
       <divMenu01 class="col-2">

f3d37aa9d666e899c3d0aa80c623b8e1f6536025
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-05-21 22:14:50 -0300

    Update memorial.php

--- memorial.php
+++ memorial.php
@@ -1,1 +1,114 @@
-<h2>memorial</h2>
+<style>
+p01{
+font: BOLD 35px Arial Black;
+color: rgb(255, 255, 255, 0.852);
+line-height: 70px;
+}
+
+p02{
+font: BOLD 120px Impact;
+color: rgb(255, 255, 255);
+line-height: 100px;
+text-decoration: underline;
+}
+
+p03{
+font: BOLD 60px Arial Black;
+color: rgb(255, 255, 255, 0.852);
+line-height: 60px;
+}
+
+p04{
+font: bold 25px arial;
+color: rgb(70, 67, 67, 1.342);
+}
+
+p05{
+font:  normal 19px arial;
+color: rgb(179, 141, 17, 0.89);
+}
+
+p06{
+font:  normal 18px arial;
+color: rgb(179, 141, 17, 0.89);
+}
+
+</style>
+
+
+<div class="container-fluind" style="background-color: rgba(42, 44, 42, 0.808); height: 450px">
+    <div class="row">
+        <div class="col-12">
+            <br><br><br><br>
+            <center>
+            <p02> 15000 </p02> 
+            <br>
+            <p01>  M O R T E S  </p01>
+            <br>
+            <p03> EM MEMORIA </p03>
+            </center>
+        </div>
+    </div>
+</div>
+<hr>
+<div class="container"> 
+    <br><br>
+    <center>
+    <p04>
+    Até agora, milhares de pessoas perderam suas famílias. 
+    <br>
+    São maridos, esposas, mães, pais, filhos, filhas e muito mais.
+    </p04>
+
+    <br><br><br>
+
+    <p05>
+    Apresentamos este trabalho para lembrar os entes queridos de milhares de famílias <br>
+    e como eles promoveram o progresso social globalmente.
+    </p05>
+    </center>
+</div>
+
+<br><br><br><br><br>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-1">
+        </div>
+
+        <div class="col-8">
+            <h3> Li Wenliang </h3>
+            <h5> 3 4  a n o s </h5>
+            <p06>
+            "Voltarei à linha de frente assim que me recuperar.
+             O coronavírus está se espalhando e não quero ser um desertor".
+            </p06>
+            <br><br><br><br><br>
+            <h3> Duan Zhengcheng </h3>
+            <h5> 8 6 a n o s </h5>
+            <p06>
+            "Na busca pela ciência, pode não haver resultados por 20 anos.
+             Mas não se pode ficar impaciente e desesperado por realizar. Temos de estabelecer uma meta e ser persistentes".
+            </p06>
+
+            <br><br><br><br><br>
+            <h3> Yu Guanrong </h3>
+            <h5> 7 1  a n o s  </h5>
+            <p06>
+            "Eu não posso assistir uma vida escapar na minha frente."
+            </p06>
+
+            <br><br><br><br><br>
+            <h3> Chen Ziqian </h3>
+            <h5> 6 5  a n o s  </h5>
+            <p06>
+            "Quando você abre meus momentos no WeChat, pode ler este poema como meu epitáfio."
+            </p06>
+
+        </div>
+
+        <div class="col-3">
+        </div>
+    </div>    
+</div>

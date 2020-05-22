adb484d272d3a6cda1bcec9e0ae1be3c547e2d89
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-05-21 22:13:28 -0300

    Update rodape.php

--- rodape.php
+++ rodape.php
@@ -1,13 +1,25 @@
 <footer>
 <style>
     p1{
-        font: normal 9pt Segoe UI Semibold;
+        font: normal 12px Arial;
         color: rgba(255, 255, 255, 0.80);
     }
 
 </style>
+
 <hr>
-    <div class="container-fluid" style="background-color: rgba(17, 23, 56, 0.685);">
+<a href ="#divTopo">
+    <div class="container-fluid" style="background-color: rgba(1, 5, 32, 0.585); height: 45px;">
+        <div class="row">
+            <div class="col-12">
+            <center>
+            <p05 style="font: bold 13px Arial; line-height: 45px; color: rgba(255, 255, 255, 0.90);"> &larr; Voltar ao início </p05> 
+            </center>
+            </div>
+        </div>
+    </div>
+</a>
+    <div class="container-fluid" style="background-color: rgba(1, 5, 32, 0.885);">
         <div class="row">
             <div class="col-12">  
                <center>
@@ -15,10 +27,9 @@
                 <br>
                 Maycon Deyvid Brito de Oliveria [&copy;OLIVEYRA]  |  Todos os direitos reservados.
                 <br> <br>
-                Contato:(83)9.9967-5920   |   E-mail: mayconbrito1998@hotmail.com
+                Contato: (83)9.9967-5920   |   E-mail: mayconbrito1998@hotmail.com
                 <br> <br>
                 </p1>
-               
                </center>
             </div>
         </div>

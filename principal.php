4222272e04b9c912b121a00ce4e9a716e01f5dff
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-06-02 18:59:30 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -1,13 +1,40 @@
 <link rel="stylesheet" href="estilos.css">
+<style>
+  p02{
+    font: bold 32px Tahoma;
+    color:  rgba(255, 255, 255, 0.999); 
+    line-height: 65px;
+    letter-spacing:1px
+   
+   }
 
+  p01{
+    font: bold 27px Bahnschrift Condensed;
+    letter-spacing:2px
+    
+   }
+
+  
+  divM01{
+  border-radius: 12px;
+  background-color: rgba(255, 255, 255, 0.938);
+  border-style: double;
+ 
+  }
+
+</style>
+
+<!-- CHAMA PAG PRINCIPAL E MENU* -->
+<?php
+		include_once('topo.php');
+		include_once('menu.php'); 
+	?>		
+		
 <hr>
-<div class="container" style="background-color: rgba(1, 5, 32, 0.05);">
+<br><br>
+<div class="container">
   <div class="row">
     <div class="col-7">
-      <div class="panel-heading">
-        <h3><a name="O que é COVID-19">O que é COVID-19</a></h3> 
-        <hr>
-      </div>
     </div>
 
     <div class="col-1">
@@ -15,7 +42,7 @@
 
     <div class="col-4">
       <div class="panel-heading">
-        <h3 style="text-align: center">Última Atualização</h3> 
+        <h3 style="text-align: center">TOTAL DE CASOS</h3> 
         <hr>
       </div>
     </div>
@@ -23,106 +50,144 @@
 
   <div class="row">
     <div class="col-7">
-      <p>   
-      A COVID-19 é uma doença causada pelo coronavírus SARS-CoV-2, que apresenta um quadro 
-      clínico que varia de infecções assintomáticas a quadros respiratórios graves. <br>
-      De acordo com a Organização Mundial de Saúde (OMS), a maioria dos pacientes com COVID-19 (cerca de 80%)
-      podem ser assintomáticos e cerca de 20% dos casos podem requerer atendimento hospitalar por 
-      apresentarem dificuldade respiratória e desses casos aproximadamente 5% podem necessitar de
-      suporte para o tratamento de insuficiência respiratória (suporte ventilatório)
-      <br><br>
+    <!--1-->
+      <?php
+        $query= mysqli_query($conexao,"SELECT * FROM  pag_principal where id_conteudo=1");
+        while ($tabela= mysqli_fetch_array($query)){
+          $titulo= $tabela['titulo_conteudo']."<hr>";
+          $conteudo= $tabela['conteudo']."<br><br><br>";
+        
+      ?>
+        <a name="index1"> <h3> <?php echo $titulo ?> </h3> </a>
+        <p> <?php echo $conteudo ?> </p>
+      <?php } ?>
 
-      <h4>O que é o coronavírus?</h4>
-      Coronavírus é uma família de vírus que causam infecções respiratórias. O novo agente do coronavírus
-      foi descoberto em 31/12/19 após casos registrados na China. Provoca a doença chamada de coronavírus 
-      (COVID-19). Os primeiros coronavírus humanos foram isolados pela primeira vez em 1937. No entanto, 
-      foi em 1965 que o vírus foi descrito como coronavírus, em decorrência do perfil na microscopia,
-      parecendo uma coroa.A maioria das pessoas se infecta com os coronavírus comuns ao longo da vida, sendo 
-      as crianças pequenas mais propensas a se infectarem com o tipo mais comum do vírus. Os coronavírus mais 
-      comuns que infectam humanos são o alpha coronavírus 229E e NL63 e beta coronavírus OC43, HKU1.
-      <br><br>
+    <!--2-->
+      <?php
+        $query= mysqli_query($conexao,"SELECT * FROM  pag_principal where id_conteudo=2");
+        while ($tabela= mysqli_fetch_array($query)){
+          $titulo= $tabela['titulo_conteudo']."<hr>";
+          $conteudo= $tabela['conteudo']."<br><br><br>";
+        
+      ?>
+        <a name="index1"> <h3> <?php echo $titulo ?> </h3> </a>
+        <p> <?php echo $conteudo ?> </p>
+      <?php } ?>
 
-      <h4><a name="Quais são os sintomas">Quais são os sintomas</a></h4>
-      Os sintomas da COVID-19 podem variar de um simples resfriado até uma pneumonia severa.
-      Sendo os sintomas mais comuns:
-      <br>
-        &#9657; Tosse <br>
-        &#9657; Febre <br>
-        &#9657; Coriza <br>
-        &#9657; Dor de garganta <br>
-        &#9657; Dificuldade para respirar <br>
-      <br><br>
+    <!--3-->
+      <?php
+        $query= mysqli_query($conexao,"SELECT * FROM  pag_principal where id_conteudo=3");
+        while ($tabela= mysqli_fetch_array($query)){
+          $titulo= $tabela['titulo_conteudo']."<hr>";
+          $conteudo= $tabela['conteudo']."<br><br><br>";
+        
+      ?>
+        <a name="index2"> <h3> <?php echo $titulo ?> </h3> </a>
+        <p> <?php echo $conteudo ?> </p>
+      <?php } ?>
 
-      <h4><a name="Como é transmitido">Como é transmitido</a></h4>
-      A transmissão acontece de uma pessoa doente para outra ou por contato próximo por meio de: 
-      <br>
-       &#9657; Toque do aperto de mão <br>
-       &#9657; Gotículas de saliva <br>
-       &#9657; Espirro <br>
-       &#9657; Tosse <br>
-       &#9657; Catarro <br>
-       &#9657; Objetos ou superfícies contaminadas, como celulares, mesas, maçanetas, brinquedos, 
-       teclados de computador etc.
-      <br><br>
+    <!--4-->
+      <?php
+        $query= mysqli_query($conexao,"SELECT * FROM  pag_principal where id_conteudo=4");
+        while ($tabela= mysqli_fetch_array($query)){
+          $titulo= $tabela['titulo_conteudo']."<hr>";
+          $conteudo= $tabela['conteudo']."<br><br><br>";
+      
+      ?>
+        <a name="index3"> <h3> <?php echo $titulo ?> </h3> </a>
+       <p> <?php echo $conteudo ?> </p>
+      <?php } ?>
 
-      <h4><a name="Diagnóstico">Diagnóstico</a></h4>
-      O diagnóstico da COVID-19 é realizado primeiramente pelo profissional de saúde que deve avaliar a
-      presença de critérios clínicos: Pessoa com quadro respiratório agudo, caracterizado por sensação 
-      febril ou febre, que pode ou não estar presente na hora da consulta (podendo ser relatada ao 
-      profissional de saúde), acompanhada de tosse OU dor de garganta OU coriza OU dificuldade respiratória,
-      o que é chamado de Síndrome Gripal. Pessoa com desconforto respiratório/dificuldade para respirar OU
-      pressão persistente no tórax OU saturação de oxigênio menor do que 95% em ar ambiente OU coloração 
-      azulada dos lábios ou rosto, o que é chamado de Síndrome Respiratória Aguda Grave
-      Caso o paciente apresente os sintomas, o profissional de saúde poderá solicitar exame laboratoriais:
-      De biologia molecular (RT-PCR em tempo real) que diagnostica tanto a COVID-19, a Influenza ou a presença 
-      e Vírus Sincicial Respiratório (VSR).
-      Imunológico (teste rápido) que detecta, ou não, a presença de anticorpos em amostras coletadas somente 
-      após o sétimo dia de início dos sintomas.
-      O diagnóstico da COVID-19 também pode ser realizado a partir de critérios como: histórico de contato 
-      próximo ou domiciliar, nos últimos 7 dias antes do aparecimento dos sintomas, com caso confirmado 
-      laboratorialmente para COVID-19 e para o qual não foi possível realizar a investigação laboratorial 
-      específica, também observados pelo profissional durante a consulta.
-      <br><br>
+    <!--5-->
+      <?php
+        $query= mysqli_query($conexao,"SELECT * FROM  pag_principal where id_conteudo=5");
+        while ($tabela= mysqli_fetch_array($query)){
+          $titulo= $tabela['titulo_conteudo']."<hr>";
+          $conteudo= $tabela['conteudo']."<br><br><br>";
+      
+      ?>
+        <a name="index4"> <h3> <?php echo $titulo ?> </h3> </a>
+        <p> <?php echo $conteudo ?> </p>
+        
+      <?php } ?>
 
-      <h4><a name="Como se proteger">Como se proteger</a></h4>
-      As recomendações de prevenção à COVID-19 são as seguintes:
-      <br>
-      &#9657; Lave com frequência as mãos até a altura dos punhos, com água e sabão, ou então higienize com álcool em gel 70%. <br>
-      &#9657; Ao tossir ou espirrar, cubra nariz e boca com lenço ou com o braço, e não com as mãos. <br>
-      &#9657; Evite tocar olhos, nariz e boca com as mãos não lavadas. <br>
-      &#9657; Ao tocar, lave sempre as mãos como já indicado. <br>
-      &#9657; Mantenha uma distância mínima de cerca de 2 metros de qualquer pessoa tossindo ou espirrando. <br>
-      &#9657; Evite abraços, beijos e apertos de mãos. Adote um comportamento amigável sem contato físico, mas sempre com um sorriso no rosto. <br>
-      &#9657; Higienize com frequência o celular e os brinquedos das crianças. <br>
-      &#9657; Não compartilhe objetos de uso pessoal, como talheres, toalhas, pratos e copos. <br>
-      &#9657; Mantenha os ambientes limpos e bem ventilados. <br>
-      &#9657; Evite circulação desnecessária nas ruas, estádios, teatros, shoppings, shows, cinemas e igrejas. Se puder, fique em casa. <br>
-      &#9657; Se estiver doente, evite contato físico com outras pessoas, principalmente idosos e doentes crônicos, e fique em casa até melhorar. <br>
-      &#9657; Durma bem e tenha uma alimentação saudável. <br>
-      &#9657; Utilize máscaras caseiras ou artesanais feitas de tecido em situações de saída de sua residência. <br>
-      </p>
+    <!--6-->
+      <?php
+        $query= mysqli_query($conexao,"SELECT * FROM  pag_principal where id_conteudo=6");
+        while ($tabela= mysqli_fetch_array($query)){
+          $titulo= $tabela['titulo_conteudo']."<hr>";
+          $conteudo= $tabela['conteudo']."<br><br><br>";
+      
+      ?>
+        <a name="index5"> <h3> <?php echo $titulo ?> </h3> </a>
+        <p> <?php echo $conteudo ?> </p>
+      <?php } ?>
+
     </div>
 
     <div class="col-1">
     </div>
+  
+    <!-- PAINEL DE CASOS-->
+    <div class="col-4" style="height: 440px; border-radius: 35px;  border-style: solid; background-color: rgba(7, 128, 175, 0.733);">
+      <?php
+        $query= mysqli_query($conexao,'SELECT * FROM dados_covid ORDER BY id_dados_covid');
+          while($tabela= mysqli_fetch_array($query)){
+            $data= $tabela['data_dados'];
+            $mortes= $tabela['mortes_dados'];
+            $recuperados= $tabela['recuperados_dados'];
+            $confirmados= $tabela['confirmados_dados'];
 
-    <div class="col-4" style="border-style: double;  height: 360px;">
+           $confirmados= number_format($confirmados, 0, '', '.');
+           $recuperados= number_format($recuperados, 0, '', '.');
+           $mortes= number_format($mortes, 0, '', '.'); # FORMATA NUMEROS (EX. 1 --> 1.00)
+          $data= date('d/m/Y',strtotime($data)); #FORMATA DATA (EX. 2020/05/25 --> 25/05/2020)
+      ?>
+
+      <?php } ?>
       <center>
-      <p01>
-      Horário <br>
-      <p02>17/05/2020 20:25</p02>
-      <hr>
-      Confirmados <br>
-      <p02>241.080</p02> 
-      <hr>
-      Recuperados <br>
-      <p02>94.122</p02> 
-      <hr>
-      Mortos <br>
-      <p02>16.118</p02> 
-      </p01>
+      <div class="container">
+      <br>
+        <div class="row">
+          <divM01 class="col-12">
+          <p01>  ATUALIZADO EM </p01> 
+          </divM01>
+        </div>
+      </div>
+      <p02><?php echo $data ?></p02>
+    
+      <div class="container">
+        <div class="row">
+          <divM01 class="col-12">
+          <p01> CASOS CONFIRMADOS </p01> 
+          </divM01>
+        </div>
+      </div>
+      <p02><?php echo $confirmados ?></p02>  
+     
+      <div class="container">
+        <div class="row">
+          <divM01 class="col-12">
+          <p01> CASOS RECUPERADOS </p01> 
+          </divM01>
+        </div>
+      </div>
+      <p02><?php echo $recuperados ?></p02> 
+     
+      <div class="container">
+        <div class="row">
+          <divM01 class="col-12">
+          <p01> ÓBITOS CONFIRMADOS </p01> 
+          </divM01>
+        </div>
+      </div>
+      <p02><?php echo $mortes?></p02> 
       <center>
-    </div>     
+    </div>  
   </div>
-
 </div>
+
+<!-- CHAMA PAG RODAPE -->
+<?php
+		include_once('rodape.php');
+?>

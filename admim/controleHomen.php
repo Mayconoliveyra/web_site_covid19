86b25db63c3496f1808971df0fbdc8a6e6e5925f
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-06-02 18:44:23 -0300

    Create controleHomen.php

---
+++ admim/controleHomen.php
@@ -0,0 +1,49 @@
+<style>
+   pMensagem{
+    color: rgb(51, 102, 161); 
+    font: bold 25px Arial;
+   }
+</style>
+   
+    <!--  INSERIR HOMENAGEADOS  -->
+    <?php include "conectaBanco.inc.php"; ?>
+
+    <?php
+
+        if(isset($_POST["alterar"])){
+            echo "<center> <pMensagem> Editado com sucesso. </pMensagem> </center>";
+            $id_memorial= $_POST['idmemorial'];
+            $nome= $_POST['nome'];
+            $sobrenome= $_POST['sobrenome'];
+            $idade= $_POST['idade'];
+            $frase= $_POST['frase'];
+            $nomeSolicitante= $_POST['nomeS'];
+            $emailSolicitante= $_POST['email'];
+            $publicar = 2; /* STATUS 2 = PUBLICADO */
+    
+            $sqlPublicar= "UPDATE memorial SET nome_mem='$nome', sobrenome_mem='$sobrenome', idade_mem='$idade', frase_mem='$frase', nome_solicitante='$nomeSolicitante', email_solicitante='$emailSolicitante', 1analise_2aceito='$publicar'  WHERE id_memorial='$id_memorial';";
+    
+            $publicar= mysqli_query($conexao, $sqlPublicar);
+    
+            if(!$publicar){
+                echo "Erro ao editar conteudo!";
+             
+            }else{
+                include_once('homenConfig.php');
+            }
+
+        } else if(isset($_POST["deletar"])){
+            echo "<center> <pMensagem> Exclúido com sucesso. </pMensagem> </center>";
+            $id_memorial= $_POST['idmemorial'];
+
+            $sqlDeletar = "DELETE FROM memorial WHERE id_memorial='$id_memorial'";
+            $deletar = mysqli_query($conexao, $sqlDeletar);
+
+            if(!$deletar){
+                echo "Erro ao excluir conteudo!";
+            }else{
+                include_once('homenConfig.php');
+            }
+        } 
+
+    ?>

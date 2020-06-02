f987b63734ae81768cb6f976c2769ff44bd3c7f9
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-06-02 18:58:57 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,34 +1,50 @@
-  
-  <div class="container-fluid" style="background-color: rgba(203, 212, 205);">
+ <style>
+ buttonMenu{
+  width: 195px;
+  height: 40px;
+ }
+
+ pMenu{ 
+  font: normal 14pt Segoe UI Semibold;
+  letter-spacing:2px
+ }
+
+ divMenuM{
+  line-height: 45px;
+ }
+ </style>
+
+
+  <div class="container-fluid" style="background-color: rgb(248, 248, 248); border-style: solid; height: 56px;">
     <div class ="row">
-    
-      <divMenu01 class="col-2">
-      <a href="/ProjetoWeb/#O%20que%20é%20COVID-19"> <pMenu01>COVID-19</pMenu01> </a>
-      </divMenu01>
+  
+      <divMenuM class="col-2">
+      <a href ="/#index1"> <buttonMenu class="btn btn-info"> <pMenu> COVID-19 </pMenu> </buttonMenu> </a>
+      </divMenuM>
 
-      <divMenu01 class="col-2">
-        <a href="/ProjetoWeb/#Quais%20são%20os%20sintomas"> <pMenu01>SITOMAS</pMenu01> </a>
-      </divMenu01>
+      <divMenuM class="col-2">
+      <a href="/#index2"> <buttonMenu class="btn btn-info"> <pMenu> SITOMAS </pMenu> </buttonMenu> </a>
+      </divMenuM>
 
-      <divMenu01 class="col-2">
-        <a href="/ProjetoWeb/#Como%20é%20transmitido"> <pMenu01>TRANSMISSÃO</pMenu01> </a>
-      </divMenu01>
+      <divMenuM class="col-2">
+      <a href="/#index3"> <buttonMenu class="btn btn-info"> <pMenu> TRANSMISSÃO </pMenu> </buttonMenu> </a>
+      </divMenuM>
 
-      <divMenu01 class="col-2">
-        <a href="/ProjetoWeb/#Diagnóstico"> <pMenu01>DIAGNÓSTICO</pMenu01> </a>
-      </divMenu01>
+      <divMenuM class="col-2">
+      <a href="/#index4"> <buttonMenu class="btn btn-info"> <pMenu> DIAGNÓSTICO </pMenu> </buttonMenu> </a> 
+      </divMenuM>
 
-      <divMenu01 class="col-2">
-        <a href="/ProjetoWeb/#Como%20se%20proteger"> <pMenu01>PREVENÇÃO</pMenu01> </a>
-      </divMenu01>
+      <divMenuM class="col-2">
+      <a href="/#index5"> <buttonMenu class="btn btn-info"> <pMenu> PREVENÇÃO </pMenu> </buttonMenu> </a>
+      </divMenuM>
 
-      <divMenu01 class="col-2">
-        <a href="?pg=memorial"> <pMenu01>MEMORIAL</pMenu01> </a>
-      </divMenu01>
-
+      <divMenuM class="col-2">
+      <a href="/?pg=memorial#memorialMortes"> <buttonMenu class="btn btn-info"> <pMenu> MEMORIAL </pMenu> </buttonMenu> </a> 
+      </divMenuM>
+      
     </div>
   </div>
- 
+
 
  
  

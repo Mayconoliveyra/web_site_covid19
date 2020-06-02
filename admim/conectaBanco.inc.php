04143053ddf42b7d7cf876f2ccc2f76d92e97fe7
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-06-02 18:42:47 -0300

    Create conectaBanco.inc.php

---
+++ admim/conectaBanco.inc.php
@@ -0,0 +1,8 @@
+<?php
+    // conectando ao servidor
+     $conexao = mysqli_connect("localhost","id13932773_maycon","9b8EmTPY7npPR.a");
+
+    // conectando ao BD criado
+    $db = mysqli_select_db($conexao, "id13932773_webcovid19");
+    
+?>

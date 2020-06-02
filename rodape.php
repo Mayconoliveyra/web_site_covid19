d6fe23a9fd99026c6f0aecbe3ef6c224078fafb9
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-06-02 19:00:05 -0300

    Update rodape.php

--- rodape.php
+++ rodape.php
@@ -1,35 +1,34 @@
 <footer>
 <style>
     p1{
-        font: normal 12px Arial;
-        color: rgba(255, 255, 255, 0.80);
+        font: bold 13px Arial;
+        color: rgba(255, 255, 255, 0.999);
+        line-height: 30px;
+        letter-spacing:1px;
     }
 
 </style>
-
-<hr>
-<a href ="#divTopo">
-    <div class="container-fluid" style="background-color: rgba(1, 5, 32, 0.585); height: 45px;">
+    <a href ="#divTopo">
+    <div class="container-fluid" style="background-color: rgba(1, 5, 32, 0.185); height: 40px;">
         <div class="row">
             <div class="col-12">
             <center>
-            <p05 style="font: bold 13px Arial; line-height: 45px; color: rgba(255, 255, 255, 0.90);"> &larr; Voltar ao início </p05> 
+            <p05 style="font: bold 13px Arial; line-height: 40px; color: rgba(26, 27, 27, 0.538);"> &larr; Voltar ao início </p05> 
             </center>
             </div>
         </div>
     </div>
-</a>
-    <div class="container-fluid" style="background-color: rgba(1, 5, 32, 0.885);">
+    </a>
+
+    <div class="container-fluid" style="background-color: rgba(1, 124, 172, 0.738); height: 110px;">
         <div class="row">
             <div class="col-12">  
                <center>
-               <p1>
+               <p1> 
                 <br>
-                Maycon Deyvid Brito de Oliveria [&copy;OLIVEYRA]  |  Todos os direitos reservados.
-                <br> <br>
-                Contato: (83)9.9967-5920   |   E-mail: mayconbrito1998@hotmail.com
-                <br> <br>
-                </p1>
+                Maycon Deyvid Brito de Oliveria [&copy;OLIVEYRA] &nbsp; | &nbsp;Todos os direitos reservados. <br>
+                Contato: (83)9.9967-5920   &nbsp; |  &nbsp;  E-mail: mayconbrito1998@hotmail.com  
+               </p1>
                </center>
             </div>
         </div>

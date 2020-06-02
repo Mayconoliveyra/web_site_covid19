1f6a12487fe079fcb73c1f90d08680a83a90af58
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-06-02 18:47:26 -0300

    Create menuConfig.php

---
+++ admim/menuConfig.php
@@ -0,0 +1,8 @@
+    <div class="container">
+    <a href="?pg=principalCovidConfig"> <h6> 1.1 - PRINCIPAL - TEXTO </h6> </a>
+    <a href="?pg=principalCasosConfig"> <h6> 1.2 - PRINCIPAL - PAINEL CASOS</h6> </a>
+    <a href="?pg=memorialTextConfig"> <h6> 2.1 - MEMORIAL - TEXTO </h6> </a>
+    <a href="?pg=solicitaçoesConfig"> <h6> 2.2 - MEMORIAL - SOLICITAÇÕES</h6> </a>
+    <a href="?pg=homenConfig"> <h6> 2.3 - MEMORIAL - HOMENAGEADOS</h6> </a>
+
+    </div>

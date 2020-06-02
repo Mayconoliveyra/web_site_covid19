e45220fc9431eddd328a458f6cd0db960dc94027
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-06-02 19:01:15 -0300

    Create voltarOuFechar.php

---
+++ voltarOuFechar.php
@@ -0,0 +1,27 @@
+<style>
+p5{
+font: bold 46px Arial;
+color: rgba(0, 0, 0, 0.587);
+
+}
+buttonVo{
+    outline-offset: 0px; 
+}
+
+
+</style>
+
+<div class="container-fluid" style="background-color: rgba(39, 34, 34, 0.00); position: fixed; top: 0px;">
+    <div class="row">
+        <div class="col-2">
+        <a href ="?pg=memorial"> <buttonVo type="buttonVo" class="btn btn-link btn-lg btn-block" style="width: 130px;">  <p5 style="line-height: 51px;"> &#8617; </p5> </buttonVo> </a> 
+        </div>
+  
+        <div class="col-8">
+        </div>
+
+        <div class="col-2" style="text-align: right;" >
+        <a href ="?pg=principal">  <buttonVo type="buttonVo" class="btn btn-link btn-lg btn-block"> <p5 style="font: bold 36px Arial;"> &#10006; </p5> </buttonVo> </a> 
+        </div>    
+    </div>
+</div>

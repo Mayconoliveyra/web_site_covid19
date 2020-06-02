5326c7de89db13a1156ae72a272694ec8da21824
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2020-06-02 18:58:14 -0300

    Create mensagemAddEnviada.php

---
+++ mensagemAddEnviada.php
@@ -0,0 +1,39 @@
+<style>
+h4{
+font: normal 29px Times New Roman;
+color:  rgba(253, 253, 253); /* COR DO TEXTO*/ 
+}
+
+p1{
+color:  rgba(253, 253, 253); /* COR DO TEXTO*/ 
+font: normal 17px Times New Roman;   
+}
+p2{
+color:  rgba(253, 253, 253); /* COR DO TEXTO*/ 
+font: normal 19px Times New Roman;  
+text-decoration: underline; 
+}
+
+</style>
+
+<div class="container-fluid" style="background-color: rgba(8, 8, 10, 0.787); height: 1100px;">
+    <div class="row">
+        <div class="col-12">
+      
+         <center>
+        <br><br><br><br>
+        <h4> Formulário enviado com sucesso. </h4>
+        <p1>
+        Muito obrigado por sua iniciativa.<br> <br>
+        Para garantir a integridade do Memorial sua mensagem será
+        analisada e verificada antes de ser publicada.
+        </p1>
+        <br><br><br><br><br>
+       
+        <a href="/?pg=memorial#memorialMortes"> <p2>Voltar para a entrada do Memorial</p2> </a>
+        </center>
+        
+        </div>
+       
+    </div>
+</div>
